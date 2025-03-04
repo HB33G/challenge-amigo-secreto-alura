845e56f0274d959a8521782622d58913858db5e7
HB33G <abg1798@gmail.com>
2025-03-04 14:08:40 -0600

    prueba funcion agregarAmigo - captura valor input

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ function agregarTextoElemento(elemento, texto){
     elementoHMTL.innerHTML = texto;
 
 }
+function agregarAmigo (){
+
+    let nombreAmigo = document.getElementById('amigo').value;
+    
+    console.log(typeof(nombreAmigo))
+    console.log (nombreAmigo)
+    console.log (nombreAmigo == '')
+}
+
+
 
 agregarTextoElemento('h1', '¡Amigo Secreto!')
 agregarTextoElemento('h2','Digita el nombre de tus amigos');
