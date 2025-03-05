efaf62c151b258df20fa3f1f8a4eb291a9d9b64a
HB33G <abg1798@gmail.com>
2025-03-05 14:24:04 -0600

    elimina espacios de la lista y validación de nombre repetido

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ function agregarTextoElemento(elemento, texto){
 
 function agregarAmigo (){
 
-    let nombreAmigo = document.getElementById('amigo').value;
+    let nombreAmigo = document.getElementById('amigo').value.trim();
 
    if (nombreAmigo === '' ){
     alert ('Ingresaste un espacio vacío, por favor ingresa un nombre valido');
-    return
+    return;
+   }
+
+   if (amigos.includes(nombreAmigo)){
+    alert(`'${nombreAmigo}' ya está en la lista.`);
+    return;
    }
 
     amigos.push(nombreAmigo);
     limpiarCaja ();
-   mostrarNombreAmigos();
-   console.log(amigos);
+    mostrarNombreAmigos();
+    console.log(amigos);
 
 }
 
@@ -45,13 +50,14 @@ function mostrarNombreAmigos (){
 
 function sortearAmigo(){
 
-    if (amigos.length == 0 ) {
+    if (amigos.length === 0 ) {
         alert('No hay nombres para sortear');
-        return
+        return;
     }
+
     let amigoAleatorio = Math.floor(Math.random()*amigos.length);
     let seleccionAmigo = amigos[amigoAleatorio];
-    
+
     agregarTextoElemento ('#resultado',`El azar ha hablado... ¡Y tu eres el elegido, ${seleccionAmigo} !`)
 
 }
